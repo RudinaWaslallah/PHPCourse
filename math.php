27f01ddf6e959079db34ac3601d1625d0d294e19
RudinaWaslallah <rudinawasx@gmail.com>
2023-10-30 22:19:03 +0300

    init

---
+++ math.php
@@ -0,0 +1,16 @@
+<?php
+echo 20*10;
+echo "<br>";
+echo 20/10;
+echo "<br>";
+echo 20-10;
+echo "<br>";
+echo 20+10;
+echo "<br>";
+echo abs(-40);
+echo "<br>";
+echo pow(5,2);
+echo "<br>";
+echo sqrt(144);
+
+?>
