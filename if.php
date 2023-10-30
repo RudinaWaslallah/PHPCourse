1d92d5a3e2b8b6cec1729fd8f6c9535edc72bd85
RudinaWaslallah <rudinawasx@gmail.com>
2023-10-30 23:16:30 +0300

    init

---
+++ if.php
@@ -0,0 +1,13 @@
+<?php
+$number1=20;
+$number2=20;
+if ($number1>$number2){
+    echo "الرقم الاول اكبر";
+}elseif($number1<$number2)
+{
+    echo "الرقم الاول اصغر";
+}else{
+   echo"الرقمين متساويين";
+}
+
+?>
