f58436904a6011d5b979ec72e9c520133634cdfd
RudinaWaslallah <rudinawasx@gmail.com>
2023-10-30 23:24:36 +0300

    init

--- if.php
+++ if.php
@@ -1,8 +1,14 @@
 <?php
 $number1=20;
-$number2=20;
+$number2=10;
 if ($number1>$number2){
     echo "الرقم الاول اكبر";
+    
+    if ($number1===20){
+        echo "<br>";
+         echo "الرقم الاول عشرون";
+    }
+
 }elseif($number1<$number2)
 {
     echo "الرقم الاول اصغر";
